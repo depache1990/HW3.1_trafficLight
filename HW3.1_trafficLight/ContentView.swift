2af4530d3d23a1424aeb1bcdf50c6e4b14f2379a
parkurru@mail.ru <parkurru@mail.ru>
2021-10-26 14:28:26 +0300

    Initial Commit

---
+++ HW3.1_trafficLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.1_trafficLight
+//
+//  Created by Anton Duplin on 26/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.1_trafficLight/HW3_1_trafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1_trafficLightApp.swift
+//  HW3.1_trafficLight
+//
+//  Created by Anton Duplin on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1_trafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
